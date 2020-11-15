de2c7ff545ded462a0947cf4610edea0a86e6dea
AngryGenie <36406335+AngryGenie@users.noreply.github.com>
2020-11-14 23:50:37 -0500

    gtr commaro

--- resources/[Cars]/16ss/__resource.lua
+++ resources/[Cars]/16ss/__resource.lua
@@ -9,6 +9,11 @@ files {
     'data/vehicles.meta', 
     'data/carcols.meta',
     'data/carvariations.meta',
+     'audioconfig/rb26dett_amp.dat10.rel',
+  'audioconfig/rb26dett_game.dat151.rel',
+  'audioconfig/rb26dett_sounds.dat54.rel',
+  'sfx/dlc_rb26dett/rb26dett.awc',
+  'sfx/dlc_rb26dett/rb26dett_npc.awc',
 --    'data/contentunlocks.meta',
 }
 
@@ -17,6 +22,10 @@ data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
 data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
 data_file 'CARCOLS_FILE' 'data/carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/rb26dett_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/rb26dett_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/rb26dett_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_rb26dett'
 -- data_file 'VEHICLE_SHOP_DLC_FILE' 'data/'
 -- data_file 'CONTENT_UNLOCKING_META_FILE' 'data/contentunlocks.meta'
 
